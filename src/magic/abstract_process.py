b01bb53c7e014d126cdd1715acf400d7080bc282
kakin <ielts_go3@163.com>
2021-04-22 14:47:11 +0800

    update: invite get_slice_point()

--- src/magic/abstract_process.py
+++ src/magic/abstract_process.py
@@ -1,13 +1,19 @@
 # coding=utf-8
 # author xin.he
+import datetime
+import numpy as np
+
 
 class AbstractProcess:
     """
     abstract process class
     """
 
-    def __init__(self):
-        pass
+    def __init__(self,
+                 width=1024,
+                 height=768):
+        self._width = int(width)
+        self._height = int(height)
 
     def process(self, loop=0):
         """
@@ -16,3 +22,14 @@ class AbstractProcess:
         :return: image object as list
         """
         raise RuntimeError('use implement class')
+
+    def get_slice_point(self):
+        """
+        generate slice point of new image
+        :return: a tuple (x, y)
+        """
+        # refresh seed
+        random_generator = np.random.RandomState(datetime.datetime.now().microsecond)
+
+        return (random_generator.randint(10, self._width, size=1)[0],
+                random_generator.randint(10, self._height, size=1)[0])
