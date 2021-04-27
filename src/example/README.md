98b60c19dc078fbf7360069c990e64036f24f8de
kakin <ielts_go3@163.com>
2021-04-27 10:11:22 +0800

    update: demo

--- src/example/README.md
+++ src/example/README.md
@@ -2,6 +2,7 @@
 
 ## Contents
 - [how to load image](howto_load_image.py)
+- [Image Mixing and Pasting Generator](howto_image_mixing_and_pasting_generator.py)
 
 ## About image Copy Right
 ALL PICTURES ARE FROM THE INTERNET, IF INFRINGEMENT, PLEASE CONTACT US TO DELETE.
---
+++ src/example/howto_image_mixing_and_pasting_generator.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+# author xin.he
+from magic import StickerPlayer
+from share import ImagesLoader
+
+# create ImagesLoader object
+fee_receipt = ImagesLoader('./images/supermarket_fee_receipt', stander_width=175)
+corgi = ImagesLoader('./images/corgi', stander_width=640, stander_height=480)
+# push ImagesLoader objects into a list
+loader_list = [fee_receipt, corgi]
+
+# call StickerPlayer to generate
+sp = StickerPlayer(loader_list=loader_list, piece=3)
+sp.process(loop=1)
